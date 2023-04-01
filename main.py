e6c223614258398a15de41949921f9330f0002fd
mik A <akimgri44@gmail.com>
2023-04-01 17:00:02 +0300

    sus

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ GREEN = '#008000'
 BLUE = '#0000FF'
 CYAN = '#00FFFF'
 
+Ball1 = ball.Ball()
+Ball2 = ball.Ball()
+Ball3 = ball.Ball()
+
+balls = [Ball1, Ball2, Ball3]
+
 img = pygame.image.load('img.png')
 img = pygame.transform.scale(img, (60, 50))
 img_rect = img.get_rect()
@@ -30,6 +36,43 @@ def draw_text(screen,text,size,x,y,color):
     text_rect.center = (0,0)
     screen.bilt(text_image, text_rect)
 
+import pygame
+
+pygame.init()
+
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+X = 400
+Y = 400
+
+display_surface = pygame.display.set_mode((X, Y))
+
+pygame.display.set_caption('Show Text')
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+text = font.render('GeeksForGeeks', True, green, blue)
+
+textRect = text.get_rect()
+
+textRect.center = (X // 2, Y // 2)
+
+
+while True:
+
+    display_surface.fill(white)
+
+    display_surface.blit(text, textRect)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+            quit()
+
+        pygame.display.update()
 platform = pygame.image.load('platform.png')
 platform_rect = platform.get_rect()
 
@@ -49,11 +92,38 @@ while run:
     key = pygame.key.get_pressed()
 
     screen.fill(CYAN)
+    for ball in balls:
+        ball.update(width, screen)
+        if platform_rect.celliderect(ball.rect):
+            score += 1
+            ball.speed[1] = -ball.speed[1]
+        if ball.rect.bottom > height:
+            rounds -= 1
+            if rounds <= 0
+                run = False
+            ball.respawn(width)
+
+
     screen.blit(img, img_rect)
     screen.blit(platform, platform_rect)
     pygame.display.update()
     img_rect.x += speedX
     img_rect.y += speedY
+
+
+    if img_rect.top < 0:
+     ping.play()
+    if img_rect.left < 0:
+     ping.play(0)
+    if img_rect.right > width:
+     ping.play( )
+
+    if img_rect.top < 0:
+        speedY = -speedY
+    if img_rect.left < 0:
+        speedX = -speedX
+    if img_rect.right > width:
+        speedX = -speedX
 
     if key[pygame.K_LEFT] and platform_rect.left > 0:
         platform_rect.x -= 10
